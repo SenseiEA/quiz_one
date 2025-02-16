43692c3086ab8a569f0fbb0a3ee4f060d691af27
Kevin Kyle C. Coraza <106863845+tarovinl@users.noreply.github.com>
2025-02-16 18:42:24 +0800

    free page added with carousel

--- lib/pages/page_free.dart
+++ lib/pages/page_free.dart
@@ -9,18 +9,39 @@ class page_free extends StatelessWidget {
         title: "Free Page Module",
         home: Scaffold(
           appBar: AppBar(
-            title:Text("Free Page Module"),
+            title:Text("Take care of your Pokemon!"),
 
           ),
-          body: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back'),
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Centers the children vertically
+              children: [
+
+                SizedBox(
+                  height :720,
+                  child: CarouselView(
+                    itemExtent: MediaQuery.sizeOf(context).width - 0,
+                    itemSnapping: true,
+                    elevation:4,
+                    padding: const EdgeInsets.all(10),
+                    children: List.generate(5,(int index){
+                      return Container(
+                          color: Colors.grey,
+                        child:Image.network('https://picsum.photos/400?random=$index',
+                        fit: BoxFit.cover,
+                        ),
+                      );
+
+                    }),
+                )
+
+                ),
+              ]
+
             ),
+
           ),
-        )
+        ),
     );
   }
 }
