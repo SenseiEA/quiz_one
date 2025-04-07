7e45caf5ab331d0bc4aeb60bc7380cb8e1dd2a22
RodPoli <142408199+RodPoli@users.noreply.github.com>
2025-04-07 09:11:21 +0800

    Pokeapi implemented

--- lib/pages/page_registration.dart
+++ lib/pages/page_registration.dart
@@ -15,6 +15,7 @@ class page_registration extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Share your Pokemon!",
       home: Scaffold(
         appBar: AppBar(
